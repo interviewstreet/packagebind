fea9e618fa3b5cc5412cd0b0420fad24a762fd7e
Aditya Agarwal <adityaa803@gmail.com>
2019-04-09 21:40:04 +0530

    Update packagebind.js

--- src/packagebind.js
+++ src/packagebind.js
@@ -1,52 +1,119 @@
-const cloneDeep = require('lodash').cloneDeep;
+const fs = require('fs');
+const path = require('path');
+const { existsSync } = require('fs');
+const { resolvePath } = require('babel-plugin-module-resolver');
+const { cloneDeep } = require('lodash');
 
-module.exports = function getBabelrcForWebpack(babelrc, aliasKeys) {
-  const cloneBabelrc = cloneDeep(babelrc);
-  const { plugins } = cloneBabelrc;
+/**
+ * given a alias path find the root path for that repo 
+ * */
+function findRepoRootPath(aliasPath) {
+  const fullAliasPath = path.resolve(aliasPath);
+  let rootPath = fullAliasPath;
 
-  const moduleAliasIndex = plugins.findIndex(
-    plugin => Array.isArray(plugin) && plugin[0] === 'module-alias'
+  while (rootPath.includes('/') && !existsSync(`${rootPath}/package.json`)) {
+    rootPath = rootPath.substring(0, rootPath.lastIndexOf('/'));
+  }
+
+  return rootPath;
+}
+
+/**
+ * given a repo path get the babel config of the linked repository
+ * */
+function getLinkedBabelrc(repoPath) {
+  if (existsSync(`${repoPath}/.babelrc`)) {
+    return JSON.parse(fs.readFileSync(`${repoPath}/.babelrc`, 'utf8'));
+  } else if (existsSync(`${repoPath}/babel.config.js`)) {
+    return require(`${repoPath}/babel.config.js`);
+  }
+  return null;
+}
+
+/**
+ * From the plugins array of a babel config extract the aliases.
+ * */
+function getAliases(plugins = []) {
+  const moduleResolver = plugins.find(
+    plugin => Array.isArray(plugin) && plugin[0] === 'module-resolver'
+  );
+  if (moduleResolver) {
+    return moduleResolver[1].alias;
+  }
+  return null;
+}
+
+
+/**
+ * Finds the repository name on which the file path exist.
+ * It returns currentRepo if the file is from the same repo, otherwise it returns the repository name
+ */
+function getFilesRepo(filePath, linkedReposMeta) {
+  const currentRepoDir = process.cwd();
+  if (filePath.startsWith(currentRepoDir)) {
+    return 'currentRepo';
+  }
+
+  return linkedReposMeta.find(({ rootPath }) => filePath.startsWith(rootPath)).name;
+}
+
+/**
+ * Get the new babel config with optimized settings for linked aliases
+ */
+function getLinkedBabelConfig(babelConfig) {
+  /** We are cloning babelrc here so we can mutate it directly to keep logic simpler */
+  const cloneBabelrc = cloneDeep(babelConfig);
+  const { plugins = [] } = cloneBabelrc;
+
+  const moduleResolver = plugins.find(
+    plugin => Array.isArray(plugin) && plugin[0] === 'module-resolver'
   );
 
-  if (moduleAliasIndex === -1) {
-    return;
-  }
+  const aliases = getAliases(plugins);
 
-  const aliases = plugins[moduleAliasIndex][1].reduce((obj, alias) => {
-    const { expose } = alias;
-    obj[expose] = alias;
-    return obj;
-  }, {});
+  const linkedRepos = Object.keys(aliases).filter((aliasKey) => aliases[aliasKey].startsWith('../'));
 
-  plugins.push([
-    'module-resolver', {
-      alias: aliasKeys.reduce((acc, key) => {
-        acc[`${key}/es`] = `${key}/es`;
-        acc[`${key}/lib`] = `${key}/es`;
-        acc[`${key}`] = `${key}/es`;
-        return acc;
-      }, {}),
-    },
-  ]);
+  const linkedReposMeta = linkedRepos.map((name) => {
+    const rootPath = findRepoRootPath(aliases[name]);
+    const babelConfig = getLinkedBabelrc(rootPath);
+    return {
+      name,
+      rootPath,
+      babelConfig,
+    };
+  });
+  
+  /** TODO: Add plugins from the linked packages as well. 
+   * For now that is not required as the plugins added on this repo is super set of all plugin used on linked repos
+   * For now just get the alias from those babel files
+   * */
 
+  const aliasMaps = {};
 
-  aliasKeys.forEach(key => {
-    delete aliases[key];
+  linkedReposMeta.forEach((meta) => {
+    const { babelConfig, name, rootPath } = meta;
+    if (babelConfig) {
+      const aliases = getAliases(babelConfig.plugins);
+      if (aliases) {
+        aliasMaps[name] = Object.entries(aliases).reduce(( updatedAliases, [key, value] ) => {
+          updatedAliases[key] = path.resolve(rootPath, value);
+          return updatedAliases;
+        }, {});
+      }
+    }
   });
 
-  const modifiedPlugins = [...plugins];
-
-  if (process.env.NODE_ENV === 'development') {
-    modifiedPlugins.push('react-hot-loader/babel');
-  }
-  modifiedPlugins[moduleAliasIndex][1] = Object.values(aliases);
-
-  const updatedBabelrc = {
-    ...cloneBabelrc,
-    cacheDirectory: false,
-    babelrc: false,
-    plugins: modifiedPlugins,
-    presets: [['es2015', { modules: false }], 'react'],
+  moduleResolver[1].resolvePath = (sourcePath, currentFile, opts) => {
+    opts = cloneDeep(opts);
+    const repo = getFilesRepo(currentFile, linkedReposMeta);
+    if (repo !== 'currentRepo' && aliasMaps[repo]) {
+      opts.alias = { ...opts.alias, ...aliasMaps[repo] };
+    }
+    const realPath = resolvePath(sourcePath, currentFile, opts);
+    return realPath;
   };
-  return updatedBabelrc;
-};
+
+  return cloneBabelrc;
+}
+
+module.exports = getLinkedBabelConfig;
